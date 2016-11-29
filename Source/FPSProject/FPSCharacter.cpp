88b4ba61d664897b608f0d068a37890f1556d3f5
Alex Nye <alnye655321@gmail.com>
2016-11-29 06:51:09 -0700

    added character mesh

--- Source/FPSProject/FPSCharacter.cpp
+++ Source/FPSProject/FPSCharacter.cpp
@@ -21,6 +21,19 @@ AFPSCharacter::AFPSCharacter()
 	// Allow the pawn to control camera rotation.
 	FPSCameraComponent->bUsePawnControlRotation = true;
 
+	// Create a first person mesh component for the owning player.
+	FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	// Only the owning player sees this mesh.
+	FPSMesh->SetOnlyOwnerSee(true);
+	// Attach the FPS mesh to the FPS camera.
+	FPSMesh->AttachTo(FPSCameraComponent);
+	// Disable some environmental shadowing to preserve the illusion of having a single mesh.
+	FPSMesh->bCastDynamicShadow = false;
+	FPSMesh->CastShadow = false;
+
+	// The owning player doesn't see the regular (third-person) body mesh.
+	GetMesh()->SetOwnerNoSee(true);
+
 }
 
 // Called when the game starts or when spawned
